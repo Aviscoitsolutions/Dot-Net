25652551a4f48cf57d346c17c64639220ecda906
Asif Nadaf <nafasif79@gmail.com>
2018-09-03 18:42:19 +0530

    Asif
    Uploaded Date:03-09-2018
    Colsole Application With solved Ignation Problem

---
+++ VTS/Console/03-09-2018 06.35PM/ExampleService/Merged/MergedData.GpsPart.cs
@@ -0,0 +1,25 @@
+﻿namespace ExampleService.Merged
+{
+    internal partial class MergedData
+    {
+        /// <remarks>size: 19 bytes</remarks>
+        public struct GpsPart
+        {
+            public float Longitude;
+
+            public float Latitude;
+
+            public byte Speed;
+
+            public byte Satellites;
+
+            public byte Hdop;
+
+            public short Direction;
+
+            public short Altitude;
+
+            public float Odometer;
+        }
+    }
+}
