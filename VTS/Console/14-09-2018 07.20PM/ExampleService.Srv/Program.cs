3bb036a644a16222acf14a53b816da4912e2ba1c
Asif Nadaf <nafasif79@gmail.com>
2018-09-14 19:33:53 +0530

    Asif
    Uploaded Date:14-09-2018
    Console Application with solving ignetion issue.

---
+++ VTS/Console/14-09-2018 07.20PM/ExampleService.Srv/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Net.Sockets;
+using System.ServiceProcess;
+
+namespace ExampleService.Srv
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        private static void Main()
+        {
+           
+            ServiceBase[] ServicesToRun;
+            ServicesToRun = new ServiceBase[]
+            {
+                new Service()
+            };
+            ServiceBase.Run(ServicesToRun);
+        }
+    }
+}
---
+++ VTS/Console/14-09-2018 07.20PM/ExampleService/Merged/MergedData.J1939Tco1Part.cs
@@ -0,0 +1,35 @@
+﻿namespace ExampleService.Merged
+{
+    internal partial class MergedData
+    {
+        /// <remarks>size: 14 bytes</remarks>
+        public struct J1939Tco1Part
+        {
+            public byte driver1_work;
+
+            public byte driver2_work;
+
+            public byte motion;
+
+            public byte driver1_time;
+
+            public byte driver1_crd;
+
+            public byte overspeed;
+
+            public byte driver2_time;
+
+            public byte driver2_crd;
+
+            public byte tcoevent;
+
+            public byte handl;
+
+            public byte perf;
+
+            public byte dir;
+
+            public ushort speed;
+        }
+    }
+}
