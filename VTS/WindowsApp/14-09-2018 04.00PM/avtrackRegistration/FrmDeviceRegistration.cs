01af76b373763d59f9e343db163c0cfd4fa57f7d
Asif Nadaf <nafasif79@gmail.com>
2018-09-14 19:35:49 +0530

    Asif
    Uploaded Date:14-09-2018
    Avtrack Registration Windows Application.

---
+++ VTS/WindowsApp/14-09-2018 04.00PM/avtrackRegistration/FrmDeviceRegistration.cs
@@ -0,0 +1,217 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ExampleService;
+namespace avtrackRegistration
+{
+    
+    public partial class FrmDeviceRegistration : Form
+    {
+        DataClasses1DataContext db = new DataClasses1DataContext();
+      
+        public FrmDeviceRegistration()
+        {
+            InitializeComponent();
+        }
+        public void clear()
+        {
+            txtDeviceName.Text = "";
+            cmbusername.Text = "--Select--";
+            txtapn.Text = "";
+            Txtsimno.Text = "";
+            Txtimei.Text = "";
+            txtDeviceModel.Text = "";
+            txtvehicletype.Text = "";
+            cmbdealer.Text = "--Select--";
+            cmbusername.Focus();
+        }
+        private void Cancel_Click(object sender, EventArgs e)
+        {
+            clear();
+        }
+
+        private void BtnSave_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                
+
+                if (cmbusername.Text == "--Select--"||cmbusername.Text=="")
+                {
+                    MessageBox.Show("Please Select User Name");
+                    cmbusername.Focus();
+                }
+                else if (txtDeviceName.Text == "")
+                {
+                    MessageBox.Show("Please Enter Device Name /Vehicle No");
+                    txtDeviceName.Focus();
+                }
+                else if (cmbdealer.Text == "--Select--" || cmbdealer.Text == "")
+                {
+                    MessageBox.Show("Please Select Dealer Name");
+                    cmbdealer.Focus();
+                }
+                else if (Txtimei.Text == "")
+                {
+                    MessageBox.Show("Please Enter Device IMEI No");
+                    Txtimei.Focus();
+                }
+               else if (Txtimei.Text.Length < 13)
+                {
+                    MessageBox.Show("Please Enter Correct IMEI No");
+                    Txtimei.Focus();
+
+                }
+                else if (txtvehicletype.Text == "")
+                {
+                    MessageBox.Show("Please Enter Vehicle Type");
+                    txtvehicletype.Focus();
+                }
+                else if (Txtsimno.Text == "")
+                {
+                    MessageBox.Show("Please Enter Sim No");
+                    Txtsimno.Focus();
+                }
+               else if (Txtsimno.Text.Length < 10)
+                {
+                    MessageBox.Show("Please Enter Correct Sim No");
+                    Txtsimno.Focus();
+
+                }
+                else if (txtapn.Text == "")
+                {
+                    MessageBox.Show("Please APN No");
+                    txtapn.Focus();
+                }
+                else if (txtDeviceModel.Text == "")
+                {
+                    MessageBox.Show("Please Device Model");
+                    txtDeviceModel.Focus();
+                }
+               
+                else
+                {
+                    var data1 = (from a in db.Tbl_DeviceRegistrations where a.DeviceIMEI == Convert.ToInt64(Txtimei.Text.Trim()) select a).ToList();
+                    if (data1.Any())
+                    {
+                        MessageBox.Show("This IMEI Already Registerd");
+                        Txtimei.Focus();
+                    }
+                    else
+                    {
+                        Tbl_DeviceRegistration t = new Tbl_DeviceRegistration();
+                        t.UserId = Convert.ToInt32(cmbusername.SelectedValue);
+                        t.DeviceIMEI = Convert.ToInt64(Txtimei.Text.Trim());
+                        t.DeviceName = txtDeviceName.Text.Trim();
+                        t.SimNo = Txtsimno.Text.Trim();
+                        t.APN = txtapn.Text.Trim();
+                        t.Date = DateTime.Now;
+                        t.DeviceModel = txtDeviceModel.Text.Trim();
+                        t.Vehicle_Type = txtvehicletype.Text.Trim();
+                        t.stetus = "1";
+                        t.Dealer_ID =Convert.ToInt32(cmbdealer.SelectedValue);
+                        db.Tbl_DeviceRegistrations.InsertOnSubmit(t);
+                        db.SubmitChanges();
+                        MessageBox.Show("Device Added Successfully");
+                        clear();
+                    }
+
+                }
+            }
+            catch (Exception)
+            {
+
+
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void txtDeviceName_KeyPress(object sender, KeyPressEventArgs e)
+        {
+          
+        }
+
+        private void Txtimei_Validated(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Txtimei_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar)
+                 && !char.IsDigit(e.KeyChar)
+                 && e.KeyChar != '.')
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void Txtsimno_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar)
+                 && !char.IsDigit(e.KeyChar)
+                 && e.KeyChar != '.')
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void Txtsimno_Validated(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void FrmDeviceRegistration_Load(object sender, EventArgs e)
+        {
+            bindusers();
+            binddealer();
+           
+        }
+        public void bindusers()
+        {
+            var data = (from a in db.Tbl_Users
+                        where a.Name != null
+                        select new
+                        {
+                            a.Name,
+                            a.UserID
+                        }).ToList();
+            cmbusername.DisplayMember = "Name";
+            cmbusername.ValueMember = "UserId";
+            cmbusername.DataSource = data;
+        }
+        public void binddealer()
+        {
+            var data1 = (from a in db.Tbl_DealerRegistrations
+
+                         select new
+                         {
+                             a.DealerName,
+                             a.Dealer_Id
+                         }).ToList();
+            cmbdealer.DisplayMember = "DealerName";
+            cmbdealer.ValueMember = "Dealer_Id";
+            cmbdealer.DataSource = data1;
+        }
+
+      
+
+     
+
+        private void txtxvehicletype_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsLetter(e.KeyChar) && !char.IsControl(e.KeyChar) && !char.IsWhiteSpace(e.KeyChar))
+                e.Handled = true;
+        }
+    }
+}
---
+++ VTS/WindowsApp/14-09-2018 04.00PM/avtrackRegistration/FrmForgotPassword.Designer.cs
@@ -0,0 +1,179 @@
+﻿namespace avtrackRegistration
+{
+    partial class FrmForgotPassword
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmForgotPassword));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtUsername = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtpassword = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.BtnSave = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel1.Controls.Add(this.button3);
+            this.panel1.Controls.Add(this.BtnSave);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.txtpassword);
+            this.panel1.Controls.Add(this.label7);
+            this.panel1.Controls.Add(this.txtUsername);
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Location = new System.Drawing.Point(7, 8);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(462, 246);
+            this.panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.LightGray;
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Location = new System.Drawing.Point(36, 10);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(369, 44);
+            this.panel2.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Black;
+            this.label1.Location = new System.Drawing.Point(115, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(143, 23);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Reset Password";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.ForeColor = System.Drawing.Color.Black;
+            this.label7.Location = new System.Drawing.Point(32, 86);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(89, 20);
+            this.label7.TabIndex = 16;
+            this.label7.Text = "User Name";
+            // 
+            // txtUsername
+            // 
+            this.txtUsername.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtUsername.Location = new System.Drawing.Point(163, 83);
+            this.txtUsername.MaxLength = 11;
+            this.txtUsername.Name = "txtUsername";
+            this.txtUsername.Size = new System.Drawing.Size(205, 26);
+            this.txtUsername.TabIndex = 15;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Black;
+            this.label2.Location = new System.Drawing.Point(32, 144);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(113, 20);
+            this.label2.TabIndex = 18;
+            this.label2.Text = "New Password";
+            // 
+            // txtpassword
+            // 
+            this.txtpassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtpassword.Location = new System.Drawing.Point(163, 141);
+            this.txtpassword.MaxLength = 11;
+            this.txtpassword.Name = "txtpassword";
+            this.txtpassword.Size = new System.Drawing.Size(205, 26);
+            this.txtpassword.TabIndex = 17;
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.White;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(340, 187);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(65, 31);
+            this.button3.TabIndex = 20;
+            this.button3.Text = "Exit";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // BtnSave
+            // 
+            this.BtnSave.BackColor = System.Drawing.Color.White;
+            this.BtnSave.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.BtnSave.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnSave.ForeColor = System.Drawing.Color.Black;
+            this.BtnSave.Location = new System.Drawing.Point(140, 187);
+            this.BtnSave.Name = "BtnSave";
+            this.BtnSave.Size = new System.Drawing.Size(149, 31);
+            this.BtnSave.TabIndex = 19;
+            this.BtnSave.Text = "Change Password";
+            this.BtnSave.UseVisualStyleBackColor = false;
+            this.BtnSave.Click += new System.EventHandler(this.BtnSave_Click);
+            // 
+            // FrmForgotPassword
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(477, 261);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "FrmForgotPassword";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Forgot Password";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtpassword;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtUsername;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button BtnSave;
+    }
+}
