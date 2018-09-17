caaf41af81a6b29c78aea45b24a28d286c8a265f
Asif Nadaf <nafasif79@gmail.com>
2018-09-17 19:03:21 +0530

    Asif
    Uploade Date:17-09-2018
    GPSINDIA Console app

---
+++ VTS/Console/GPSIndia 17-09-2018 07.00PM/ExampleService.Con/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading;
+
+namespace ExampleService.Con
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Server server = new Server();
+            server.Start();
+            while (server.IsActive())
+            {
+                var stopString = Console.ReadLine();
+
+                if (stopString != null && stopString.ToLower() == "stop")
+                {
+                    server.Stop();
+                }
+
+                Thread.Sleep(500);
+            }
+        }
+    }
+}
